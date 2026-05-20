171cb7b3a11256f93ffb47bb505d8a23ff17ea5e
J-Karp-01 <jola.karp4510@gmail.com>
2026-05-20 13:05:04 +0200

    Add project structure

---
+++ utils/loaders.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import streamlit as st
+
+
+@st.cache_data
+def load_my_data(path):
+
+    with open(path, 'r') as f:
+        lines = f.readlines()
+
+    data_start = next(
+        i for i, line in enumerate(lines)
+        if line.strip().startswith("0")
+    )
+
+    df = pd.read_csv(
+        path,
+        skiprows=data_start,
+        sep=r"\s+",
+        header=None,
+        names=["czas", "ecg1", "ecg2"],
+        engine="python"
+    )
+
+    df = df.replace(',', '.', regex=True)
+    df = df.astype(float)
+    df = df.dropna()
+
+    return df
